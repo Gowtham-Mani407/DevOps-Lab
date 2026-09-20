3b867bec0476fb570717d6bdb265f96d145a7f58
Gowtham-Mani407 <gowtham2kid@gmail.com>
2026-09-20 02:36:57 -0700

    dockercomposeWithvolume

--- docker/docker-compose-demo/app.py
+++ docker/docker-compose-demo/app.py
@@ -1,16 +1,58 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from pathlib import Path
 
-class Handler(BaseHTTPRequestHandler):
+DATA_DIR = Path("/data")
+DATA_DIR.mkdir(parents=True, exist_ok=True)
+
+DATA_FILE = DATA_DIR / "message.txt"
+
+
+class MyHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
 
-        self.wfile.write(b"Hello from Docker Compose!")
+        if self.path == "/write":
 
-server = HTTPServer(("0.0.0.0", 8000), Handler)
+            DATA_FILE.write_text(
+                "Hello Gowtham! This data is stored in Docker Volume.\n"
+            )
 
-print("Server running on port 8000")
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(
+                b"Data written to Docker volume successfully!"
+            )
+
+        elif self.path == "/read":
+
+            if DATA_FILE.exists():
+
+                content = DATA_FILE.read_text()
+
+                self.send_response(200)
+                self.end_headers()
+                self.wfile.write(content.encode())
+
+            else:
+
+                self.send_response(404)
+                self.end_headers()
+                self.wfile.write(
+                    b"No data found in volume."
+                )
+
+        else:
+
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(
+                b"Welcome to Docker Volume Demo!"
+
+            )
+
+
+server = HTTPServer(("0.0.0.0", 8000), MyHandler)
+
+print("Server running on port 8000...")
 
 server.serve_forever()
